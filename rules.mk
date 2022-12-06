5570f60275c883023094ebb5b7fe15f946a9afc0
malte.horst <maltehorst@gmail.com>
2022-12-06 18:26:33 +0100

    Fix syntax errors

--- rules.mk
+++ rules.mk
@@ -1,9 +1,9 @@
-// Add custom keymap
+# Add custom keymap
 SRC += source.c
 
-// Switch to symmetric eager per key debouncing for faster responses
+# Switch to symmetric eager per key debouncing for faster responses
 DEBOUNCE_TYPE = sym_eager_pk
 
-// Switch to integrated QMK RGB matrix control
+# Switch to integrated QMK RGB matrix control
 RGB_MATRIX_ENABLE = yes
 RGB_MATRIX_DRIVER = custom
