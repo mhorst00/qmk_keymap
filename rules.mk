684d9bb3ed5a8002d591977bd7089c5c0073679a
malte.horst <maltehorst@gmail.com>
2022-12-06 18:23:20 +0100

    Switch to RGB matrix feature

--- rules.mk
+++ rules.mk
@@ -1,2 +1,9 @@
+// Add custom keymap
 SRC += source.c
+
+// Switch to symmetric eager per key debouncing for faster responses
 DEBOUNCE_TYPE = sym_eager_pk
+
+// Switch to integrated QMK RGB matrix control
+RGB_MATRIX_ENABLE = yes
+RGB_MATRIX_DRIVER = custom
