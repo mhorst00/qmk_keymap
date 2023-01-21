79ebb9933a894b04013b93779c66bb39f590553d
malte.horst <maltehorst@gmail.com>
2023-01-21 15:59:58 +0100

    switched to eager defer pk

--- rules.mk
+++ rules.mk
@@ -1,8 +1,8 @@
 # Add custom keymap
 SRC += source.c
 
-# Switch to symmetric eager per key debouncing for faster responses
-DEBOUNCE_TYPE = sym_eager_pk
+# Switch to symmetric eager defer per key debouncing for faster responses
+DEBOUNCE_TYPE = asym_eager_defer_pk
 
 # Switch to integrated QMK RGB matrix control
 RGB_MATRIX_ENABLE = yes
